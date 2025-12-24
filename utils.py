f9a52cdd1f48cdf4e810263ac989f5947ca9cfbb
Athos <fathos82@gmail.com>
2025-12-24 14:57:41 -0300

    feat: fixing auto registering device.

--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
+import json
+import os
 import socket
 import uuid
 from pathlib import Path
 import requests
 
-DEVICE_ID_FILE = "/etc/plantmonitor/device_id"
 
 def get_or_create_device_id():
+    DEVICE_ID_FILE = "/etc/plantmonitor"
     path = Path(DEVICE_ID_FILE)
     if path.exists():
         return path.read_text().strip()
-
+    os.makedirs(os.path.dirname(DEVICE_ID_FILE), exist_ok=True)
     device_id = str(uuid.uuid4())
     path.write_text(device_id)
     return device_id
@@ -19,14 +21,18 @@ def get_or_create_device_id():
 
 def register_device():
     print("registering device on api")
+    print("generating uuid device: ")
+    # todo: verifica se ja não existe registro
+    uuid = get_or_create_device_id()
+    print("uuid: ", uuid)
     payload = {
-        "deviceUid": get_or_create_device_id(),
-        "name": "Sem Nome",
+        "deviceUid": uuid,
+        "name": "Sem Nome", # todo: alterar isso
         "hostname":  socket.gethostname(),
     }
-    url = 'http://192.168.0.107:8080/api/device/register'
-    response = requests.get(url)
-    pass
+    url = 'http://192.168.0.107:8080/api/devices/'
+    response = requests.post(url, json=payload)
+    print(response.json())
 
 
 
