2882b6fcb8720d3eb1ce744bbe7e036bbfad696d
Athos <fathos82@gmail.com>
2026-01-06 17:14:32 -0300

    feat: permitindo sicronizar sensor apartir dos registros do backend.

---
+++ sensors/sensor_factory.py
@@ -0,0 +1,24 @@
+from sensors.sensors import SensorModel
+
+
+class SensorFactory:
+
+    _REGISTRY = {}
+
+    def register(self, sensor_type: SensorModel, sensor_cls):
+        self._REGISTRY[sensor_type] = sensor_cls
+
+    def create(self, sensor_data: dict):
+        try:
+            sensor_type = SensorModel(sensor_data["model"])
+        except ValueError:
+            raise ValueError(f"Modelo inválido recebido da API: {sensor_data['model']}")
+
+        if sensor_type not in self._REGISTRY:
+            raise ValueError(f"Sensor não suportado: {sensor_type}")
+        sensor_cls = self._REGISTRY[sensor_type]
+        sensor = sensor_cls(**sensor_data.get("parameters", {}))
+        sensor.api_id = sensor_data.get("id")
+        return sensor
+
+sensor_factory = SensorFactory()
