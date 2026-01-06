8eab734ea5eb4ed00a1fc5419211b4b74581348c
Athos <fathos82@gmail.com>
2026-01-06 19:35:38 -0300

    feat: adding stop events to stop runners

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # todo: problema com GPIO -> https://chatgpt.com/c/695d3a4d-83a0-8332-9ff5-78f43c82bce9
 # Alternativa: sudo apt install python3.13-dev
-
+import random
+import threading
 import time
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timezone
@@ -11,8 +12,8 @@ from sensors.sensors import AbstractSensor
 from settings import log
 from utils import register_or_get_device, generate_qrcode_to_set_account, send_data
 SLEEP_TIME = 0.5
-def run(sensor: AbstractSensor):
-    while True:
+def run(sensor: AbstractSensor, stop_event: threading.Event):
+    while not stop_event.is_set():
         try:
             value = sensor.read()
             dt = datetime.now(timezone.utc)
@@ -23,9 +24,14 @@ def run(sensor: AbstractSensor):
             # todo: verificar integridade, caso não integro reportar ao backend.
             pass
 
-def run_device_thread():
+def run_device_thread(stop_event: threading.Event):
     while True:
-        pass
+
+        random_float= random.random()
+        if random_float < 0.1:
+            stop_event.set()
+            print("PARANDO...")
+
 
 
 
@@ -37,11 +43,14 @@ def main():
         log(message="Encerrando programa...",  context=CRITICAL)
     sensor_pool = SensorPool() # todo: totalmente substituivel para programação funcional
     sensor_pool.discover(device_uuid=device_uuid)
-
+    stop_event = threading.Event()
+    device_thread = threading.Thread(target=run_device_thread, args=(stop_event,))
+    device_thread.start()
     while True:
         with ThreadPoolExecutor(max_workers=len(sensor_pool.sensors)) as executor:
-            executor.map(run, sensor_pool.sensors)
-        print("CRIANDO")
+            for sensor in sensor_pool.sensors:
+                executor.submit(run, sensor, stop_event)
+
 
 
 if __name__ == '__main__':
