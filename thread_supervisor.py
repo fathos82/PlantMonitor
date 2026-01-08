277c01550dcb7b93446ef54a73ab7d452b9fe0d5
Athos <fathos82@gmail.com>
2026-01-08 12:19:15 -0300

    feat: logs.

--- thread_supervisor.py
+++ thread_supervisor.py
@@ -7,8 +7,6 @@ class ThreadSupervisor:
         self.runners = {}
 
     def add_sensor(self, sensor:AbstractSensor):
-        print("Adding sensor {}".format(sensor.sensor_name))
-        print("Sensor ID: {}".format(sensor.api_id))
         runner = SensorRunner(sensor)
         self.runners[sensor.local_id] = runner
         runner.start()
@@ -19,10 +17,9 @@ class ThreadSupervisor:
         if runner:
             runner.commands.put(Command.STOP)
 
-    def reload_sensor(self, local_id):
-        print("Reloading", local_id)
-        runner = self.runners.get(local_id)
-        print("RUNNER COLETADO: ", runner)
+    def reload_sensor(self, sensor):
+
+        runner = self.runners.get(sensor.api_id)
         if runner:
             runner.commands.put(Command.RELOAD)
 
