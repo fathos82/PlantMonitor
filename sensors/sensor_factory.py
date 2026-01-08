e271f3085e0d068aac8ecf29a1de82f33c30e79b
Athos <fathos82@gmail.com>
2026-01-08 14:04:21 -0300

    feat: fixing errors.

--- sensors/sensor_factory.py
+++ sensors/sensor_factory.py
@@ -10,13 +10,15 @@ class SensorFactory:
         self._REGISTRY[sensor_type] = sensor_cls
 
     def create(self, sensor_data: dict):
+        #todo: talvez futuramente reportar esse error para api!
+        #todo: associar erro ao sensor solicitado pelo ID
         try:
             sensor_type = SensorModel(sensor_data["model"])
         except ValueError:
             log(f"Modelo inválido recebido da API: {sensor_data['model']}", level="error")
 
         if sensor_type not in self._REGISTRY:
-            raise ValueError(f"Sensor não suportado: {sensor_type}")
+            log(f"Sensor não suportado: {sensor_type}", level="error")
         sensor_cls = self._REGISTRY[sensor_type]
         sensor = sensor_cls(**sensor_data.get("parameters", {}))
         sensor.api_id = sensor_data.get("id")
