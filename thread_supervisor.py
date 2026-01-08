55aac9d11c4b08aea51c1be56d24199423c485dd
Athos <fathos82@gmail.com>
2026-01-08 12:22:43 -0300

    feat: fixing supervisor

--- thread_supervisor.py
+++ thread_supervisor.py
@@ -7,22 +7,19 @@ class ThreadSupervisor:
         self.runners = {}
 
     def add_sensor(self, sensor:AbstractSensor):
-        print("Adding sensor {}".format(sensor.sensor_name))
-        print("Sensor ID: {}".format(sensor.api_id))
         runner = SensorRunner(sensor)
-        self.runners[sensor.local_id] = runner
+        self.runners[sensor.api_id] = runner
         runner.start()
         runner.commands.put(Command.START)
 
-    def remove_sensor(self, local_id):
-        runner = self.runners.pop(local_id, None)
+    def remove_sensor(self, sensor):
+        runner = self.runners.pop(sensor.api_id, None)
         if runner:
             runner.commands.put(Command.STOP)
 
     def reload_sensor(self, sensor):
 
         runner = self.runners.get(sensor.api_id)
-        print("RUNNER COLETADO: ", runner)
         if runner:
             runner.commands.put(Command.RELOAD)
 
