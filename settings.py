e8d990e8995e8dbdac0e587b0a06510699b6365e
Athos <fathos82@gmail.com>
2026-04-07 17:42:17 -0300

    feat: refactoring all code.

--- settings.py
+++ settings.py
@@ -20,15 +20,30 @@ SENSOR_ERROR_API_URL = SENSOR_API_URL +"{id}/errors/"
 PUBLISHER_BATCH_SIZE = int(os.getenv("PUBLISHER_BATCH_SIZE", 10))
 PUBLISHER_BATCH_INTERVAL = float(os.getenv("PUBLISHER_BATCH_INTERVAL", 5.0))  # segundos
 
-LOG_RULES = {
-    "SYSTEM": logging.DEBUG,
-    "SENSOR_POOL": logging.DEBUG,
-    "SENSOR_WORKER": logging.DEBUG,
-    "DEVICE": logging.DEBUG,
-    "CAMERA": logging.DEBUG,
-    "API_CLIENT": logging.DEBUG,
-    "MQTT": logging.DEBUG,
-    "PUBLISHER": logging.DEBUG,
+import os
+import logging
+
+DEFAULT_LOG_RULES = {
+    "SYSTEM": logging.INFO,
+    "SENSOR_POOL": logging.INFO,
+    "SENSOR_WORKER": logging.INFO,
+    "DEVICE": logging.INFO,
+    "CAMERA": logging.INFO,
+    "API_CLIENT": logging.INFO,
+    "MQTT": logging.INFO,
+    "PUBLISHER": logging.INFO,
 }
 
+def parse_log_level(level_str):
+    return getattr(logging, level_str.upper(), logging.DEBUG)
 
+LOG_RULES = {}
+
+for key, default in DEFAULT_LOG_RULES.items():
+    env_var = f"LOG_LEVEL_{key}"
+    level_str = os.getenv(env_var)
+
+    if level_str:
+        LOG_RULES[key] = parse_log_level(level_str)
+    else:
+        LOG_RULES[key] = default
